1e6e93857dd01a9170fa2bd0c0fe5018dad9b013
Ben Wager <benwager@gmail.com>
2020-09-24 10:07:37 +0200

    Added methods to clear metadataframe buffer from metadatasender

--- Assets/Test/MetadataSender.cs
+++ Assets/Test/MetadataSender.cs
@@ -26,4 +26,19 @@ public class MetadataSender : MonoBehaviour
         sender.sendAudioFrameMetadata = audioFrameMetaData;
         sender.sendMetadataFrameData = metadataFrameData;
     }
+
+    public void ClearVideFrameMetadata()
+    {
+        videoFrameMetaData = null;
+    }
+
+    public void ClearAudioFrameMetadata()
+    {
+        audioFrameMetaData = null;
+    }
+
+    public void ClearMetadataFrameMetadata()
+    {
+        metadataFrameData = null;
+    }
 }
